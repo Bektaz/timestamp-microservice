63d9f4a793422b0bbe3ea6a84bde1dc42051842b
Bereket <berekettaz@gmail.com>
2016-01-11 17:35:37 +0500

    3d test commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 var port = process.env.PORT || 8080;
 
 var server = http.createServer(function(req, res){
+    /*
     var obj = url.parse(req.url, true), data;
     if(obj.pathname==='/api/parsetime'){  
         var date = new Date(obj.query.iso); 
@@ -20,7 +22,13 @@ var server = http.createServer(function(req, res){
         }
         res.writeHead(200, {'content-type': 'application/json'});
         res.end(JSON.stringify(data));
+    }else if(obj.pathname==='/'){
+        res.end('Hello this is my first app in Heroku!!!');
     }
+    */
+    res.writeHead(200, {'content-type': 'text/html'});
+    var html = fs.readFileSync(__dirname+'/index.htm');
+    res.end(html);
 });
 server.listen(port, function(){
 	console.log('Our app is running on http://localhost:'+port);
